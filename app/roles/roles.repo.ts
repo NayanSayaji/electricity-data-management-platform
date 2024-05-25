de893130afdda9fc0225f741c8f07557c48bcb3d
nayan-coditas <nayan.sayaji@coditas.com>
2024-05-25 22:56:34 +0530

    Added - refining folder structure

---
+++ app/roles/roles.repo.ts
@@ -0,0 +1,10 @@
+import rolesModel from '../roles/roles.schema'
+
+const findRoleById = (query: string) => {
+    try {
+        const result = rolesModel.findByPk(query);
+        return result;
+    } catch (e) {
+        throw e
+    }
+}
